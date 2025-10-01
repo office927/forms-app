02a4e4bd7505302b23f1e5f0427c1767628cb686
office927 <office@conquesteng.us>
2025-10-01 08:33:32 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'forms-cache-v1';
+const urls = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(urls))));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
